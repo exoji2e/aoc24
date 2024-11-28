57f070864c1c941e448a7cce36ad79de3370e8ec
Måns Magnusson <exoji2e@gmail.com>
2024-11-28 23:16:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent of Code 2024
+
+My solutions to [advent of code 2024](https://adventofcode.com/2024).
+
+If you wish to run them yourself:
+
+install deps with `python3 -m pip install -r requirements.txt`.
+
+Download your session cookie (from dev tools - storage/application) and create a file secret.py with the contents:
+`session = '<your session cookie>'`.
+
+Run each solution as a normal python file: `python3 solved/01/day01.py`.
---
+++ template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys, time, datetime
+sys.path.extend(['..', '.'])
+from collections import *
+from runner import main, get_commands
+from utils import *
+def get_day(): return datetime.date.today().day
+def get_year(): return 2024
+
+def parse(ln):
+    return lazy_ints(multi_split(ln, ' '))
+
+def p1(v):
+    lns = get_lines(v)
+    chunks = v.split('\n\n')
+    ans = 0
+    for ln in lns:
+        item = parse(ln)
+        ans += 1
+    return ans
+
+def p2(v):
+    return p1(v)
+
+
+if __name__ == '__main__':
+    options = get_commands()
+    print('Commands:', options)
+    main(get_year(), get_day(), p1, p2, options, FILE=__file__)
